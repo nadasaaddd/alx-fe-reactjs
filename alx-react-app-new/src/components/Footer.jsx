e4ee7b65f4a9fe8f98e7bd0198d4eb3c680a9795
Nada Saad <nadasaaddd@gmail.com>
2025-07-27 16:33:43 +0300

    task1.3

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer
+      tyle={{ border: "1px solid gray", padding: "10px", margin: "10px" }}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main tyle={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
