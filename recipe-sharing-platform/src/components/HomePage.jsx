573a889f5c48c7b8d0cb5d081fa2accee61dac0e
Nada Saad <nadasaaddd@gmail.com>
2025-08-15 17:30:53 +0300

    task2.1

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import recipeData from "../data.json";
+"Link", "react-router-dom";
 
 const HomePage = () => {
   const [recipes, setRecipes] = useState([]);
